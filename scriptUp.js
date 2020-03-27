971b65246368868096f6f6db0bb09adf9755419a
Andrei Shakh <andreishakhs@gmail.com>
2020-03-27 12:12:35 +0300

    lesson02

---
+++ scriptUp.js
@@ -0,0 +1,16 @@
+let num = 266219;
+let arr = [];
+num = num.toString();
+
+for (let i of num) {
+    arr.push(Number(i));
+}
+
+let result = arr.reduce((result, currentValue) => result * currentValue);
+
+result = result ** 3;
+
+console.log(result);
+console.log(result.toString()[0], result.toString()[1]);
+
+
