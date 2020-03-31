f76f3ac6509f8216c985c1e7e6acc3211250e47b
Andrei Shakh <andreishakhs@gmail.com>
2020-03-31 11:17:04 +0300

    lesson04

--- scriptUp.js
+++ scriptUp.js
@@ -1,30 +1,12 @@
-'use strict';
-
-function day(lang) {
-    if (lang === 'ru') {
-       console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-    } else {
-        console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
+const foo = function (arg) {
+    if (typeof(arg) !== 'string') {
+        alert('Введите строку');
     }
-}
+    let str = arg.trim();
+    if (str.length >= 30) {
+        str = str.substring(0, 30) + '...';
+    }
+    return str;
+};
 
-day('ru');
-
-let lang ='en';
-switch (lang) {
-    case 'ru':
-        console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-        break;
-    case 'en':
-        console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
-        break;
-}
-
-
-let days = [['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'], ['Понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']];
-console.log('en', days[1]);
-console.log('ru', days[1]);
-
-let namePerson = 'Вася';
-let person = namePerson === 'Артем' ? console.log('Директор') : namePerson === 'Максим' ? console.log('Преподаватель') : console.log('студент');
-
+console.log(foo('  foo0000000000000000000000000002132132131   '));
