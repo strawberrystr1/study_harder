e4e6eb0d59c462c811f4308a3178497c6a4a4ea1
Andrei Shakh <andreishakhs@gmail.com>
2020-04-01 12:36:01 +0300

    lesson05

--- scriptUp.js
+++ scriptUp.js
@@ -1,12 +1,15 @@
-const foo = function (arg) {
-    if (typeof(arg) !== 'string') {
-        alert('Введите строку');
+let arr = ['23234','5235','412','123144','24141','123','4444'];
+for(let i=0; i<arr.length; i++) {
+    if (arr[i].charAt(0) === '2' || arr[i].charAt(0) === '4') {
+        console.log(arr[i]);
     }
-    let str = arg.trim();
-    if (str.length >= 30) {
-        str = str.substring(0, 30) + '...';
-    }
-    return str;
-};
+}
 
-console.log(foo('  foo0000000000000000000000000002132132131   '));
+let n =100;
+prime:
+for (let i=0; i < n; i++) {
+    for(let j=2; j < i; j++) {
+        if (i%j==0) continue prime;
+    }
+    console.log(`Делители этого числа ${i}: 1 и ${i}`);
+}
