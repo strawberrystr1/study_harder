8fe54168ca241008fdd59ba8c2f3dedef8101c61
Andrei Shakh <andreishakhs@gmail.com>
2020-04-05 15:03:39 +0300

    lesson07

--- scriptUp.js
+++ scriptUp.js
@@ -1,15 +1,19 @@
-let arr = ['23234','5235','412','123144','24141','123','4444'];
-for(let i=0; i<arr.length; i++) {
-    if (arr[i].charAt(0) === '2' || arr[i].charAt(0) === '4') {
-        console.log(arr[i]);
+let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let date = new Date().getDay();
+
+
+for (let i = 0; i < week.length; i++) {
+    if (i === date) {
+        if (week[i] == 'Sunday' || week[i] == 'Saturday') {
+            document.write(`<p><b><i>${week[i]}</i></b></p>`);
+        } else {
+            document.write(`<p><b>${week[i]}</b></p>`);
+        }
+    } else if (week[i] == 'Saturday' || week[i] == 'Sunday') {
+        document.write(`<p><i>${week[i]}</i></p>`);
+    } else {
+        document.write(`<p>${week[i]}</p>`);
     }
 }
 
-let n =100;
-prime:
-for (let i=0; i < n; i++) {
-    for(let j=2; j < i; j++) {
-        if (i%j==0) continue prime;
-    }
-    console.log(`Делители этого числа ${i}: 1 и ${i}`);
-}
+
